6932b9ffddbad34a396e69d03dad54589eccf25d
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-08 01:18:22 +0300

    created a squad object

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,32 +1,32 @@
-//import java.util.ArrayList;
-//
-//public class Squad {
-//    private String name;
-//    private  String cause;
-//    private int maxsize;
-//    private static ArrayList<Squad>mInstances=new ArrayList<>();
-//
-//    public Squad(String name, String cause,int maxsize){
-//        this.name=name;
-//        this.cause=cause;
-//        this.maxsize=maxsize;
-//        this.mInstances.add(this);
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getCause() {
-//        return cause;
-//    }
-//
-//    public void setMaxsize(int maxsize) {
-//        this.maxsize = maxsize;
-//    }
-//
-//    public static ArrayList<Squad> getmInstances() {
-//        return mInstances;
-//    }
-//}
-//
+import java.util.ArrayList;
+
+public class Squad {
+    private String name;
+    private  String cause;
+    private int maxsize;
+    private static ArrayList<Squad>mInstances=new ArrayList<>();
+
+    public Squad(String name, String cause,int maxsize){
+        this.name=name;
+        this.cause=cause;
+        this.maxsize=maxsize;
+        this.mInstances.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setMaxsize(int maxsize) {
+        this.maxsize = maxsize;
+    }
+
+    public static ArrayList<Squad> getmInstances() {
+        return mInstances;
+    }
+}
+
