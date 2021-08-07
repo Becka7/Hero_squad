53d6c37070632db49671f9143752a0103888f791
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-08 00:58:22 +0300

    created app.java

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Squad {
-}
+//import java.util.ArrayList;
+//
+//public class Squad {
+//    private String name;
+//    private  String cause;
+//    private int maxsize;
+//    private static ArrayList<Squad>mInstances=new ArrayList<>();
+//
+//    public Squad(String name, String cause,int maxsize){
+//        this.name=name;
+//        this.cause=cause;
+//        this.maxsize=maxsize;
+//        this.mInstances.add(this);
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getCause() {
+//        return cause;
+//    }
+//
+//    public void setMaxsize(int maxsize) {
+//        this.maxsize = maxsize;
+//    }
+//
+//    public static ArrayList<Squad> getmInstances() {
+//        return mInstances;
+//    }
+//}
+//
