63d10ee86debd4fa6ab2c661e4b1f15381c01d55
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-08 02:15:49 +0300

    added a getter

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,19 @@
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
 
         staticFileLocation("/public");
+
+        get("/",((request, response) -> {
+            Map<String,Object>model = new HashMap<String,Object>();
+            return new ModelAndView(new HashMap(),"squad.hbs")
+
+        }));
     }
 
 }
