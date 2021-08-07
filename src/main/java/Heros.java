1c89c65271a303b65c0b3507ee1685f19c64ca41
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-08 01:17:02 +0300

    created a hero object

--- src/main/java/Heros.java
+++ src/main/java/Heros.java
@@ -1,30 +1,30 @@
-//import java.util.ArrayList;
-//
-//public class Heros {
-//    private String name;
-//    private int age;
-//    private String power;
-//    private String weakness;
-//    private static ArrayList<Heros>mInstance= new ArrayList<>();
-//
-//
-//    public Heros(String name,int age ,String power,String weakness){
-//        this.name=name;
-//        this.age=age;
-//        this.power=power;
-//        this.weakness=weakness;
-//        this.mInstance.add(this);
-//
-//    }
-//    public String getName() {return name;}
-//
-//    public int getAge(){ return age;}
-//
-//    public String getPower(){ return power;}
-//
-//    public String getWeakness() {return weakness;}
-//
-//    public static ArrayList<Heros> getAll(){return mInstance;}
-//
-//
-//}
+import java.util.ArrayList;
+
+public class Heros {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private static ArrayList<Heros>mInstance= new ArrayList<>();
+
+
+    public Heros(String name,int age ,String power,String weakness){
+        this.name=name;
+        this.age=age;
+        this.power=power;
+        this.weakness=weakness;
+        this.mInstance.add(this);
+
+    }
+    public String getName() {return name;}
+
+    public int getAge(){ return age;}
+
+    public String getPower(){ return power;}
+
+    public String getWeakness() {return weakness;}
+
+    public static ArrayList<Heros> getAll(){return mInstance;}
+
+
+}
